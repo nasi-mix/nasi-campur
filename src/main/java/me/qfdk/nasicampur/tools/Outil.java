809fafe12670b7956796f03a180eaf649b9d2b6e
qfdk <qfdk2010@gmail.com>
2018-10-07 10:56:43 +0200

    [Fix] delete

--- src/main/java/me/qfdk/nasicampur/tools/Outil.java
+++ src/main/java/me/qfdk/nasicampur/tools/Outil.java
@@ -1,20 +1,11 @@
 package me.qfdk.nasicampur.tools;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.net.ServerSocket;
-import java.util.Base64;
 
 public class Outil {
-    public static String getSSRUrl(String host,String port, String password) throws UnsupportedEncodingException {
-        String pass = Base64.getEncoder().encodeToString(password.getBytes("utf-8")).replace("=", "");
-//        ssr://base64(host:port:protocol:method:obfs:base64pass
-        String tmp = host+":"+port+":origin:rc4-md5:plain:" + pass;
-        String str = Base64.getEncoder().encodeToString(tmp.getBytes("utf-8")).replace("=", "");
-        return str;
-    }
 
-    public static String getRandomPort(){
+    public static String getRandomPort() {
         // 生成随机可用端口
         ServerSocket s = null;
         try {
@@ -22,7 +13,7 @@ public class Outil {
         } catch (IOException e) {
             System.err.println("没有可用随机端口");
         }
-        String port=String.valueOf(s.getLocalPort());
+        String port = String.valueOf(s.getLocalPort());
         try {
             s.close();
         } catch (IOException e) {
@@ -31,7 +22,7 @@ public class Outil {
         return port;
     }
 
-    public static String getPass(String str){
+    public static String getPass(String str) {
         return new StringBuffer(str).reverse().toString();
     }
 }
