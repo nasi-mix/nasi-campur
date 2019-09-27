a4d67d0211204bc14c11d7c2050015a9550038d3
qfdk <qfdk2010@gmail.com>
2019-09-27 20:28:41 +0200

    [TEST]

--- src/main/java/me/qfdk/nasicampur/service/PontService.java
+++ src/main/java/me/qfdk/nasicampur/service/PontService.java
@@ -27,15 +27,15 @@ public class PontService {
             session.setServerAliveInterval(10000);
             session.connect();
             // 设置SSH本地端口转发,本地转发到远程
-            int assinged_port = session.setPortForwardingL(localPort, remoteHost, remotePort);
+//            int assinged_port = session.setPortForwardingL(localPort, remoteHost, remotePort);
             // 设置SSH远程端口转发,远程转发到本地
-//        session.setPortForwardingR(remotePort, remoteHost, localPort);
+            session.setPortForwardingR(remotePort, remoteHost, localPort);
             session.setDaemonThread(true);
             // 删除本地端口的转发
             // session.delPortForwardingL(localPort);
             // 断开SSH链接
             // session.disconnect();
-            log.info("[端口转发]添加 localhost:" + assinged_port + " -> " + remoteHost + ":" + remotePort);
+            log.info("[端口转发]添加 localhost:" + localPort + " -> " + remoteHost + ":" + remotePort);
         } catch (Exception e) {
             e.printStackTrace();
             if (session != null) {
